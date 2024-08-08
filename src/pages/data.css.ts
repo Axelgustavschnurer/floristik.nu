fcf6a88405ccf5f06c7e8a5982569897efc341a1
Axel <axel.schnurer@gmail.com>
2024-08-09 00:10:04 +0200

    Transition bugfix

--- src/pages/data.css.ts
+++ src/pages/data.css.ts
@@ -18,7 +18,7 @@ export async function GET() {
 
     let css: string = ''
     for (let i = 0; i < longestArray; i++) {
-        css += `.image-slider-container-container:has(section label:nth-child(${i + 1}) > input[type="radio"]:checked) > grid img {transform: translate(-${i * 100}%, 0)}`
+        css += `.image-slider-container-container:has(section label:nth-child(${i + 1}) > input[type="radio"]:checked) > flex img {transform: translate(-${i * 100}%, 0)}`
     }
 
     return new Response( css , {
