38703ed606208b275b9e341c2c013ef270518fb9
Axel <axel.schnurer@gmail.com>
2024-08-13 14:19:38 +0200

    Add svelte integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-    image: {
-        remotePatterns: [{
-          protocol: "https"
-        }]
-    },
+  image: {
+    remotePatterns: [{
+      protocol: "https"
+    }]
+  },
+  integrations: [svelte()]
 });
